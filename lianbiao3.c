d407ed626841b29eb14088f40e842b142271f15e
root <root@DESKTOP-MKGVBPJ.localdomain>
2020-11-21 13:04:03 +0800

    first commit

---
+++ lianbiao3.c
@@ -0,0 +1,14 @@
+#include "lianbiao1.h"
+
+int main(int argc,char **argv) {
+	List *list = (List *)malloc(sizeof(list));
+	initList(list);
+	addHead(list,4);
+	addHead(list,6);
+	addHead(list,8);
+	addHead(list,10);
+	dispList(list);
+	printf("the list:%d\n",getLength(list));
+
+	return 0;
+} 
